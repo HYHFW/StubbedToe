9d0d489d654848e031581572075e2a5146f07021
HYHFW <60275798+HYHFW@users.noreply.github.com>
2020-01-25 09:26:30 +1100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<head>
+<title>Test CSS Switching</title>
+<link rel="stylesheet" type="text/css" href="default.css" title="default"/>
+<link rel="stylesheet" type="text/css" href="Water.css" title="water"/>
+<link rel="stylesheet" type="text/css" href="Fire.css" title="fire"/>
+<link rel="stylesheet" type="text/css" href="Earth.css" title="earth"/>
+<script type="text/javascript" src="styleswitch.js"></script>
+</head>
+
+<body>
+<a href="#" onclick="setActiveStyleSheet('default'); return false;">Change style to default (yellow)</a><br><br>
+<a href="#" onclick="setActiveStyleSheet('water'); return false;">Change style to Water (blue)</a><br><br>
+<a href="#" onclick="setActiveStyleSheet('fire'); return false;">Change style to Fire (red)</a><br><br>
+<a href="#" onclick="setActiveStyleSheet('earth'); return false;">Change style to Earth (green)</a><br><br>
+
+
+<!--<p class="text">Hello</p>-->
+
+</body>
